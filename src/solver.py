190de6275e028d1f3da45feada3ab1e10c6f7275
excaliburrr <taaseen05@gmail.com>
2026-03-26 10:14:39 +0100

    Maybe fixed fvm problem

--- src/solver.py
+++ src/solver.py
@@ -33,9 +33,9 @@ def iterrarion_loop(r, t, W_init, D, S, V, bc_first, bc_last):
     """
     lr = r.size
     lt = t.size
-
+    print(str(lt) + "*" + str(lr))
     if lr != W_init.size:
-        raise ValueError("'r' and '_W_init' have different size")
+        raise ValueError("'r' and '_W_init' have different shape")
 
     D = make_time_dependent(D,lt)
     S = make_time_dependent(S,lt)
@@ -49,18 +49,18 @@ def iterrarion_loop(r, t, W_init, D, S, V, bc_first, bc_last):
     if V.shape != (lt, lr):
         raise ValueError("'V' must have shape lt*lr. Current shape is: " + str(V.shape))
 
-    print(r.size)
-    print(W_init.size)
-
     # callculate the step lengths
-    # FIXME d_r is distances betwean flux surfaces and should be given as input params to this function
+    # FIXME "d_r is distances betwean flux surfaces and should be given as input params to this function"
+    # is d_r really the distance between faces? I don't think so, it should be the distance between centers.
     d_r = np.diff(r)
     d_t = np.diff(t)
+    """
     # dr_volume is the width of a cell, while d_r is the width between centers
     dr_volume = np.zeros(lr)
     dr_volume[1:-1] = 0.5 * (d_r[:-1] + d_r[1:])
     dr_volume[0] = 0.5 * d_r[0]
     dr_volume[-1] = 0.5 * d_r[-1]
+    """
 
     # Initilise the kinetic energy matrix
     W_size = len(t), len(r)
@@ -71,6 +71,7 @@ def iterrarion_loop(r, t, W_init, D, S, V, bc_first, bc_last):
     # FIXME d_r[0] is lazy solution
     # FIXME use neuman conditions
     # Changed the range, for t that has length lt, there are lt - 1 faces
+
     for i in range(lt - 1):
 
         """
@@ -78,49 +79,37 @@ def iterrarion_loop(r, t, W_init, D, S, V, bc_first, bc_last):
         D_average = 2 / (1 / D[0:-1] + 1 / D[1:])
         """
         # take next values, for euler backwards
+        # this is correct, we take the next value in time.
+        # Is diffusivity given at faces or taken at faces? Do we know the diffusivity of a cell, or through the cell to its neighbours?
+        # FIXME
         D_next = D[i + 1,:]
         S_next = S[i + 1,:]
         V_next = V[i + 1,:]
-
-        VD = V_next*D_next
+        # VD is taken at a face now
+        VD = V_next * D_next
         VD_average = 2 / (1 / VD[0:-1] + 1 / VD[1:])
-        """
-        Also removing this since S is an array now, and we enforce the bc by setting bc_first/bc_last      
-        # No sources at boundaryconditions, will disturb these conditions!
-        S[0] = 0
-        S[-1] = 0
-        """
+
         P = np.zeros((lr,lr))
         RHS = np.zeros(lr)
         P[0,0] = 1
         P[-1,-1] = 1
         RHS[0] = bc_first
         RHS[-1] = bc_last
+        rf_distance = 0.5 * (r[:-1] + r[1:])
 
-        """
-        P = (
-            np.diag(
-                V / d_t[i]
-                + np.concatenate(([0], D_average[1:] + D_average[:-1], [0])) / d_r[0] ** 2
-            )
-            + np.diag(-np.concatenate((D_average[1:], [0])) / d_r[0] ** 2, k=-1)
-            + np.diag(-np.concatenate(([0], D_average[1:])) / d_r[0] ** 2, k=1)
-        )
-        """
-        
         # Now technically, I shouldn't have **2 for a finite volume scheme.
         # But since everything is assumed to be uniform, it is fine. If we use DREAM inputs, then this has to be changed
-        # FIXME
+        # FIXME maybe fixed?
         for j in range(1, lr-1):
-            a_left = VD_average[j-1]/(dr_volume[j-1] ** 2)
-            a_right = VD_average[j]/(dr_volume[j] ** 2)
+            a_left = VD_average[j-1]/(d_r[j-1])
+            a_right = VD_average[j]/(d_r[j])
 
             P[j,j-1] = -a_left
-            P[j,j]= V_next[j]/d_t[i] + a_left + a_right
+            P[j,j]= (V_next[j] * rf_distance[j-1])/d_t[i] + a_left + a_right
             P[j,j+1] = -a_right
 
             # Calculate the rhs based on current cell
-            RHS[j] = V_next[j] * (S_next[j] + W[i,j] / d_t[i])
+            RHS[j] = V_next[j] * rf_distance[j-1] * (S_next[j] + W[i,j] / d_t[i])
 
 
         # Do some reasonable checks
