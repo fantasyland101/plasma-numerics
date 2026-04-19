Added average energy graph and calculations Fixed boundary issues Fixed initial profile

src/main.py
@@ -1,42 +1,46 @@
-import h5py
 import matplotlib.pyplot as plt
 import numpy as np
+import h5py
 
 from solver import iterrarion_loop
 
-file_path = "dream_outputs.h5"
+file_path = 'dream_outputs.h5'
 
-with h5py.File(file_path, "r") as f:
-    E_para = f["eqsys/E_field"][:]
-    E_ceff = f["other/fluid/Eceff"][:]
-    j_re = f["eqsys/j_re"][:]
-    r = f["grid/r"][:]
-    W_i = f["eqsys/W_i"][:]
+with h5py.File(file_path, 'r') as f:
+    E_para = f['eqsys/E_field'][:]
+    E_ceff = f['other/fluid/Eceff'][:]
+    j_re = f['eqsys/j_re'][:]
+    r = f['grid/r'][:]
+    n_re = f['eqsys/n_re'][:]
 
 N = 22
 tt = 1919
 
 # Calculate s from input data.
-# E_para and j_re have shape (1919, 22) while E_ceff has shape (1918, 22)
+# E_para and j_re have shape (1919, 22) while E_ceff has shape (1918, 22
 S = (E_para[0:-1] - E_ceff) * j_re[0:-1]
-D_0 = np.array([0.3] * N)
-D = np.repeat(D_0[np.newaxis, :], tt, axis=0)
-V_0 = np.array([1] * N)
-V = np.repeat(V_0[np.newaxis, :], tt, axis=0)
+D = np.full((tt, N), 0.5)
+V = np.full((tt, N), 1.0)
 
-# Time grid
+
+# grid
 t = np.linspace(0, 0.2, tt)
-
+d_r = np.diff(r)
+d_t = np.diff(t)
 
 # Initial value of W at t_min
-W_init = W_i[0, 0, :]
-bc_first = 1
-bc_last = 1
+W_init = d_t[0] * (E_para[0, :] - E_ceff[0, :]) * j_re[0, :]
+
+# These are the neumann conditions, and are therefore saying dW/dr=bc_first or bc_last
+bc_first = 0
+bc_last = 0
 
 
 def main():
-    W = iterrarion_loop(r, t, W_init, D, S, V, bc_first, bc_last)
+    W, W_avg_timeslice = iterrarion_loop(r, t, d_r, d_t, W_init, D, S, V, n_re, bc_first, bc_last)
     renderer(r, t, W)
+    renderer2(t, W_avg_timeslice)
+    plt.show()
 
 
 def renderer(r, t, W):
@@ -44,8 +48,14 @@ def renderer(r, t, W):
     ax = plt.figure().add_subplot(projection="3d")
     a, b = np.meshgrid(r, t)
     ax.plot_surface(a, b, W)
-    plt.show()
+# my lazy fix becaus
+def renderer2(t, W_avg_timeslice):
+    plt.figure()
+    plt.plot(t, W_avg_timeslice)
 
+    plt.xlabel("Time")
+    plt.ylabel("Average energy")
+    plt.grid(True)
 
 if __name__ == "__main__":
     main()

src/solver_dual.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def iterrarion_loop(r, t, d_r, d_t, W_init, D, S, V, n_re, bc_first, bc_last):
+def iterrarion_loop(r, t, W_init, D, S, V, bc_first, bc_last):
     """Using finite volume method to solve a pde of the form
     (all partial derivatives) V(r)* dW/dt = d/dr(V(r)*D(r) * dW/dr ) + *V(r)*K(r).
     Parameters:
@@ -11,9 +11,11 @@ def iterrarion_loop(r, t, d_r, d_t, W_init, D, S, V, n_re, bc_first, bc_last):
         Time grid; shape: lt
     W_init : np.ndarray
         Initial condition at t = 0, shape: lr
-        First and last values are Neumann boundary conditions
+        First and last values are Dirichlet boundary values
     D, S, V : np.ndarray
-        Diffusion, source and flux surface coefficients; shape: lt*lr
+        Diffusion, source and flux surface coefficients.
+        These can be passed as either a list (time-independent) or an array (time-dependent)
+        The function make_time_dependent takes a coefficient and pastes it to make a lr*lt array
 
     Returns
     -------
@@ -39,8 +41,11 @@ def iterrarion_loop(r, t, d_r, d_t, W_init, D, S, V, n_re, bc_first, bc_last):
             f"'V' must have shape lt*lr=({(lt, lr)}). Current shape is: " + str(V.shape)
         )
 
+    # callculate the step lengths
     # FIXME "d_r is distances betwean flux surfaces and should be given as input params to this function"
     # is d_r really the distance between faces? I don't think so, it should be the distance between centers.
+    d_r = np.diff(r)
+    d_t = np.diff(t)
 
     # dr_volume is the width of a cell, while d_r is the width between centers
     dr_volume = np.zeros(lr)
@@ -48,56 +53,72 @@ def iterrarion_loop(r, t, d_r, d_t, W_init, D, S, V, n_re, bc_first, bc_last):
     dr_volume[0] = 0.5 * d_r[0]
     dr_volume[-1] = 0.5 * d_r[-1]
 
-    # Initialise the solution matrix
-    W = np.full((lt, lr), np.nan)
-    W_init[0] = W_init[1] - bc_first * d_r[0]
-    W_init[-1] = W_init[-2] + bc_last * d_r[-1]
-
+    # Initilise the kinetic energy matrix
+    W_size = len(t), len(r)
+    W = np.full(W_size, np.nan)
     W[0, :] = W_init
-    W[0] = bc_first
-    W[-1] = bc_last
 
-    W_avg_timeslice = np.zeros(lt)
     P = np.zeros((lr, lr))
     RHS = np.zeros(lr)
+    # FIXME d_r[0] is lazy solution
+    # FIXME use neuman conditions
     for i in range(lt - 1):
+        """
+        Need to change this since we are supposed to take the harmonic average over V*D not just D
+        D_average = 2 / (1 / D[0:-1] + 1 / D[1:])
+        """
+        # take next values, for euler backwards
+        # this is correct, we take the next value in time.
+        # Is diffusivity given at faces or taken at faces? Do we know the diffusivity of a cell, or through the cell to its neighbours?
+        # FIXME
         D_next = D[i + 1, :]
         S_next = S[i + 1, :]
         V_next = V[i + 1, :]
-
+        # VD is taken at a face now
         VD = V_next * D_next
         VD_average = 2 / (1 / VD[0:-1] + 1 / VD[1:])
 
+        P[0, 0] = 1
+        P[-1, -1] = 1
+        RHS[0] = bc_first
+        RHS[-1] = bc_last
+
+        # ALT SOL
         A_left = VD_average[:-1] / (d_r[:-1] * dr_volume[1:-1])
         A_right = VD_average[1:] / (d_r[1:] * dr_volume[1:-1])
         A_center = V_next[1:-1] / d_t[i] + A_left + A_right
-        
-        P = (
+        _P = (
             np.diag(np.concatenate(([0], A_center, [0])), k=0)
             + np.diag(np.concatenate(([0], -A_right)), k=1)
             + np.diag(np.concatenate((-A_left, [0])), k=-1)
         )
+        _P[0, 0] = 1
+        _P[-1, -1] = 1
 
-        RHS = V_next * S_next + (V_next * W[i, :]) / d_t[i]
+        _RHS = V_next * S_next + W[i, :] / d_t[i]
+        # ALT SOL END
 
+        # Now technically, I shouldn't have **2 for a finite volume scheme.
+        # But since everything is assumed to be uniform, it is fine. If we use DREAM inputs, then this has to be changed
+        # FIXME maybe fixed?
+        for j in range(1, lr - 1):
+            a_left = VD_average[j - 1] / (d_r[j - 1] * dr_volume[j])
+            a_right = VD_average[j] / (d_r[j] * dr_volume[j])
 
-        P[0, :] = 0 
-        P[0, 0] = -1
-        P[0, 1] = 1
-        RHS[0] = bc_first * d_r[0]
+            P[j, j - 1] = -a_left
 
-        P[-1, :] = 0
-        P[-1, -2] = -1
-        P[-1, -1] = 1
-        RHS[-1] = bc_last * d_r[-1]
+            P[j, j] = (V_next[j]) / d_t[i] + a_left + a_right
+            P[j, j + 1] = -a_right
+
+            # Calculate the rhs based on current cell
+            RHS[j] = V_next[j] * (S_next[j] + W[i, j] / d_t[i])
+        print(P == _P)
+        # Do some reasonable checks
+        if np.isnan(P).any():
+            print("error here in P")
+
         W[i + 1, :] = np.linalg.solve(P, RHS)
-        W_total = np.sum(W[i, :] * V[i, :] * dr_volume)
-        N_total = np.sum(n_re[i, :] * V[i, :] * dr_volume)
-        W_avg_timeslice[i] = W_total / N_total
+
         if np.isnan(W[i + 1, :]).any():
             raise ValueError("The resulting matrix contains NAN, should not happen!")
-    W[-1, :] = W[-2, :]
-    W_total = np.sum(W[-1, :] * V[-1, :] * dr_volume)
-    N_total = np.sum(n_re[-1, :] * V[-1, :] * dr_volume)
-    W_avg_timeslice[-1] = W_total / N_total
-    return W, W_avg_timeslice
+    return W